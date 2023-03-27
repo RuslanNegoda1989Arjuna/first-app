6c9bc39ad740a4c8d7982494b9b37c1bdb744577
Ruslan Negoda <103960102+RuslanNegoda1989Arjuna@users.noreply.github.com>
2023-03-28 00:36:54 +0300

    1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 
-import './App.css';
+import './shared/styles/styles.scss';
+import Navbar from './modules/Navbar/Navbar.jsx';
+
+import navbarMenuItems from "./data/navbarMenuItems.json"
 
 function App() {
   return (
     <div className="App">
       <h1>Hello react</h1>
+      <Navbar menuItems={navbarMenuItems} />
     </div>
   );
 }
