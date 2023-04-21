b84ffa2cd502d2723d9ab5abc228c2edb1d3f60b
Ruslan Negoda <103960102+RuslanNegoda1989Arjuna@users.noreply.github.com>
2023-04-21 19:48:09 +0300

    4

--- src/modules/GoodsList/GoodsList.jsx
+++ src/modules/GoodsList/GoodsList.jsx
@@ -3,13 +3,15 @@ import goodsList from '../../data/goodsList.json';
 import styles from './GoodsList.module.scss'
     
 const GoodsList = () => {
-    const [isBold, setIsBold] = useState(true);
-    
+    const [isBold, setIsBold] = useState(0);
+
     return (
         <div className={styles.container}>
             <ul>
-                {goodsList.map(({ id, productName }) => {
-                    return (<li className={styles.item} key={id}>
+                {goodsList.map(({ id, productName }, index) => {
+                  const fullClassName = index === isBold ? `${styles.item} ${styles.active}` : `${styles.item}`
+
+                    return (<li className={fullClassName} key={id} onClick={() => setIsBold(index)}>
                         <p>{ productName}</p>
                     </li>)
                 })}
