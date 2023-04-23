685234d752bd076ec9e25a68921ca46ec2836dce
Ruslan Negoda <103960102+RuslanNegoda1989Arjuna@users.noreply.github.com>
2023-04-23 20:21:59 +0300

    4 fix

--- src/modules/TabList/TabList.jsx
+++ src/modules/TabList/TabList.jsx
@@ -4,9 +4,9 @@ import tabLost from "../../data/tabList.json";
 import styles from './tabList.module.scss';
 
 const TabList = () => {
-  const [isActive, setIsActive] = useState(1);
+  const [isActive, setIsActive] = useState("1");
+  const filteredData = tabLost.find((item) => item.id === isActive);
 
-  // const filteredData = tabLost.find((item) => item.id === id);
 
     return (
         <div className={styles.tabs}>            
@@ -19,27 +19,15 @@ const TabList = () => {
             </li>)
           })}
         </ul>
-        {tabLost.map(({ id, paragraph }) => {
-          
-            return (
-              <div key={id} className={`${styles.tabsContent} ${styles.active}`}>
-                {Object.values(paragraph).map((text, index) => {
-                  return(<p className={styles.paragr} key={index}>{text}</p>) 
-                })  }                                   
-              </div>
-              )
-            
-          })}
+         {filteredData && (
+        <div className={`${styles.tabsContent} ${styles.active}`}>
+          {Object.values(filteredData.paragraph).map((text, index) => (
+            <p className={styles.paragr} key={index}>{text}</p>
+          ))}                                   
+        </div>
+      )}
         </div>
 )
 }
 
 export default TabList
-
-//  {filteredData && (
-//         <div className={`${styles.tabsContent} ${styles.active}`}>
-//           {Object.values(filteredData.paragraph).map((text, index) => (
-//             <p className={styles.paragr} key={index}>{text}</p>
-//           ))}                                   
-//         </div>
-//       )}
