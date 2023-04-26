c6b30b3abcee580461d1ab44d2c8777d037b83d8
Ruslan Negoda <103960102+RuslanNegoda1989Arjuna@users.noreply.github.com>
2023-04-26 22:15:51 +0300

    123

--- src/modules/RegisterForm/RegisterForm.jsx
+++ src/modules/RegisterForm/RegisterForm.jsx
@@ -1,5 +1,6 @@
 import { Formik, Field, Form } from 'formik';
 import * as yup from 'yup';
+import styles from './registerForm.module.scss'
 
 const validationSchema = yup.object().shape({
   name: yup.string()
@@ -13,8 +14,8 @@ const validationSchema = yup.object().shape({
 });
 
 const RegisterForm = () => {  
-   return (<div>
-        <h2>Registration</h2>
+   return (<div className={styles.mainFormBox}>
+        <h2 className={styles.title}>Registration</h2>
         <Formik
             initialValues={{
                 name: '',
@@ -26,23 +27,32 @@ const RegisterForm = () => {
                     console.log(values)
                 }}>
                 {({ errors, touched }) => (
-                    <Form>
-                        <label htmlFor='name'>Name</label>
-                        <Field id="name" name="name" placeholder="Name" />
+               <Form className={styles.formBox}>
+                   <div className={styles.fieldWrapper}>
+                      <Field className={styles.field} id="name" name="name" placeholder="Name" />
                         {errors.name && touched.name ? (
-                           <div>{errors.name}</div>
-                        ) : null}
-                        <label htmlFor='email'>Email</label>
-                        <Field id="email" name="email" placeholder="Email" />
+                           <div className={styles.error}>{errors.name}</div>
+                        ) : null} 
+                   </div>
+                   <div className={styles.fieldWrapper}>
+                        <Field className={styles.field} id="email" name="email" placeholder="Email" />
                         {errors.email && touched.email ? (
-                           <div>{errors.email}</div>
+                           <div className={styles.error}>{errors.email}</div>
                         ) : null}
-                        <label htmlFor='password'>Name</label>
-                        <Field id="password" name="password" placeholder="Password" />
+                   </div>
+                   <div className={styles.fieldWrapper}>
+                        <Field className={styles.field} id="password" name="password" placeholder="Password" />
                         {errors.password && touched.password ? (
-                           <div>{errors.password}</div>
+                           <div className={styles.error}>{errors.password}</div>
                         ) : null}
-                    <button type='submit'>Submit</button>
+                   </div>
+                       
+                        
+                       
+                       
+            
+                       
+                    <button className={styles.submitBtn} type='submit'>Submit</button>
                     </Form>
             )}
             
