8efe0ac1c3d1fe0af5c2e95fc782773983990850
Ruslan Negoda <103960102+RuslanNegoda1989Arjuna@users.noreply.github.com>
2023-07-15 12:48:07 +0300

    shopping form start

--- src/modules/ShoppingForm/ShoppingForm.jsx
+++ src/modules/ShoppingForm/ShoppingForm.jsx
@@ -1,9 +1,65 @@
-import styles from './shoppingForm.module.scss'
+import styles from './shoppingForm.module.scss';
+import { Formik, Field, Form } from 'formik';
+import * as yup from 'yup';
 
-const ShoppingForm = () => {
+
+const validationSchemaShopList = yup.object().shape({
+  name: yup.string().required('Введіть назву товару'),
+  number: yup.number().required('Введіть кількість').positive('Кількість має бути позитивним числом'),
+  price: yup.number().required('Введіть ціну').positive('Ціна має бути позитивним числом'),
+  urgentPurchase: yup.boolean().required('Виберіть терміновість покупки'),
+  typeOfPurchase: yup.string().oneOf(['household chemicals', 'products', 'other'], 'Виберіть тип покупки'),
+});
+
+const shoppingValues = {
+    name: '',
+    number: '',
+    price: '',
+    urgentPurchase: false,
+    typeOfPurchase: '',
+}
+
+const ShoppingForm = ({ onClick }) => {
+    const handleSubmit = (values, { resetForm }) => {
+        console.log(values)
+        onClick(values);
+        resetForm();
+    }
+    
     return (
-        <div>
-            <p>Form</p>
+        <div className={styles.mainFormBox}>
+            <h2 className={styles.title}>Registration</h2>
+            <Formik
+                initialValues={shoppingValues}
+                validationSchema={validationSchemaShopList}
+                onSubmit={handleSubmit}
+            >
+               {({ errors, touched }) => (
+         <Form>
+            <div className={styles.fieldWrapper}>
+                <Field className={styles.field} id="name" name="name" type="text" placeholder="Name" />
+                {errors.name && touched.name ? (<div className={styles.error}>{errors.name}</div>) : null} 
+            </div>
+            <div className={styles.fieldWrapper}>
+                <Field className={styles.field} id="number" name="number" type="number" placeholder="Number" />
+                {errors.number && touched.number ? (<div className={styles.error}>{errors.number}</div>) : null} 
+            </div>
+            <div className={styles.fieldWrapper}>
+                <Field className={styles.field} id="price" name="price" type="number" placeholder="Price" />
+                {errors.price && touched.price ? (<div className={styles.error}>{errors.price}</div>) : null} 
+            </div>
+                        
+            <div className={styles.fieldWrapper}>
+                <Field className={styles.field} id="urgentPurchase" type="checkbox"
+        name="urgentPurchase" />
+                {errors.urgentPurchase && touched.urgentPurchase ? (<div className={styles.error}>{errors.urgentPurchase}</div>) : null} 
+            </div>
+
+
+            <button className={styles.submitBtn} type='submit'>Submit</button>
+         </Form>
+       )}
+            </Formik>
 
         </div>
     )
--- src/pages/ShoppingListPage/ShoppingListPage.jsx
+++ src/pages/ShoppingListPage/ShoppingListPage.jsx
@@ -1,14 +1,23 @@
 import ShoppingForm from "../../modules/ShoppingForm/ShoppingForm"
 import ShoppingList from "../../modules/ShoppingList/ShoppingList"
 import styles from './shoppingListPage.module.scss'
+import { nanoid } from 'nanoid';
+
+import { useState } from "react"
 
 const ShoppingListPage = () => {
+    const [shoppingList, setShoppingList] = useState([]);
+
+    const handleSubmit = (shoppingItem) => {
+        shoppingItem.id = nanoid();
+        setShoppingList(prevList => [...prevList, shoppingItem])
+    }
     return (
         <div className='container Sun'>
             <h2 className='pageTitle'>Shopping List</h2>
             <div className={styles.shopingBox}>
                 <ShoppingList />
-                <ShoppingForm/>
+                <ShoppingForm onClick={handleSubmit} />
             </div>
             
         </div>
